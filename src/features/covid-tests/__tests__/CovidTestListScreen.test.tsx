4816f3cf5b32f8bf9d560511d34a0c91d341fb44
Ben Fox <deathbycornishpastie@hotmail.com>
2021-08-12 10:46:11 +0100

    Unit tests for onboarding modal (#1583)
    * detox build script
    
    * Unit test for showing onboarding modal
    
    * typo

--- src/features/covid-tests/__tests__/CovidTestListScreen.test.tsx
+++ src/features/covid-tests/__tests__/CovidTestListScreen.test.tsx
@@ -1,4 +1,6 @@
 /* eslint-env jest */
+import { assessmentCoordinator } from '@covid/core/assessment/AssessmentCoordinator';
+
 import { Text } from '@covid/components';
 import CovidTestListScreen from '@covid/features/covid-tests/CovidTestListScreen';
 import { theme } from '@covid/themes';
@@ -8,27 +10,58 @@ import { Provider as ReduxProvider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import createMockStore from 'redux-mock-store';
 import { ThemeProvider } from 'styled-components/native';
-
-import { initialState } from '../../../../__mocks__/mockedInitialState';
 import MockedNavigator from '../../../../__mocks__/MockedNavigator';
+import CovidTestListOnboardingModal from '../modals/CovidTestListOnboardingModal';
 
 const middlewares = getDefaultMiddleware();
 const mockStore = createMockStore(middlewares);
-const store = mockStore(initialState);
 
 describe('CovidTestListScreen tests', () => {
   it('renders conditional introductory copy', () => {
+    jest.spyOn(assessmentCoordinator, 'isReportedByOther').mockReturnValue(false);
     const elementBase = <MockedNavigator Component={CovidTestListScreen} />;
+    const store = mockStore({ content: { startupInfo: { show_covid_test_onboarding: true } } });
     const elementWithRedux = <ReduxProvider store={store}>{elementBase}</ReduxProvider>;
     const element = <ThemeProvider theme={theme}>{elementWithRedux}</ThemeProvider>;
     const instance = renderer.create(element).root;
-
     // NOTE: It seems that no covid tests are loading from service for this case.
     // TODO: Can't get the mocking of the CovidTestService call to work, so I can set whether a test has been logged by a user and check alternative.
     expect(instance.findByProps({ testID: 'covid-test-list-introduction' }).props.children).toContain(
       'If you’ve ever had a COVID-19 test',
     );
-
     expect(instance.findAllByType(Text).map((t) => t.props.children)).not.toContain('Test type');
+  });
+
+  it('shows onboarding modal for primary user', () => {
+    jest.spyOn(assessmentCoordinator, 'isReportedByOther').mockReturnValue(false);
+    const elementBase = <MockedNavigator Component={CovidTestListScreen} />;
+    const store = mockStore({ content: { startupInfo: { show_covid_test_onboarding: true } } });
+    const elementWithRedux = <ReduxProvider store={store}>{elementBase}</ReduxProvider>;
+    const element = <ThemeProvider theme={theme}>{elementWithRedux}</ThemeProvider>;
+    const instance = renderer.create(element).root;
+    const modal = instance.findByType(CovidTestListOnboardingModal);
+    expect(modal).toBeTruthy();
+  });
+
+  it('does not show onboarding modal when flag is false', () => {
+    jest.spyOn(assessmentCoordinator, 'isReportedByOther').mockReturnValue(false);
+    const elementBase = <MockedNavigator Component={CovidTestListScreen} />;
+    const store = mockStore({ content: { startupInfo: { show_covid_test_onboarding: false } } });
+    const elementWithRedux = <ReduxProvider store={store}>{elementBase}</ReduxProvider>;
+    const element = <ThemeProvider theme={theme}>{elementWithRedux}</ThemeProvider>;
+    const instance = renderer.create(element).root;
+    const modals = instance.findAllByType(CovidTestListOnboardingModal);
+    expect(modals.length).toEqual(0);
+  });
+
+  it('does not show onboarding modal for none primary user (reported_by_another: true)', () => {
+    jest.spyOn(assessmentCoordinator, 'isReportedByOther').mockReturnValue(true);
+    const elementBase = <MockedNavigator Component={CovidTestListScreen} />;
+    const store = mockStore({ content: { startupInfo: { show_covid_test_onboarding: true } } });
+    const elementWithRedux = <ReduxProvider store={store}>{elementBase}</ReduxProvider>;
+    const element = <ThemeProvider theme={theme}>{elementWithRedux}</ThemeProvider>;
+    const instance = renderer.create(element).root;
+    const modals = instance.findAllByType(CovidTestListOnboardingModal);
+    expect(modals.length).toEqual(0);
   });
 });
