07fc9fc126bcc217f885173343a160940c76c541
Julia Tan <juliatan@users.noreply.github.com>
2021-09-10 16:06:34 +0100

    Add button and API call (#1674)
    * Add button and API call
    
    * Use loading prop of BrnadedButton

--- src/features/multi-profile/SelectProfileScreen.tsx
+++ src/features/multi-profile/SelectProfileScreen.tsx
@@ -1,17 +1,25 @@
 import { NUMBER_OF_PROFILE_AVATARS } from '@assets';
+import { BrandedButton } from '@covid/components';
 import { Screen } from '@covid/components/Screen';
 import { HeaderText, SecondaryText } from '@covid/components/Text';
+import { assessmentCoordinator } from '@covid/core/assessment/AssessmentCoordinator';
 import { Coordinator, IEditableProfile, ISelectProfile } from '@covid/core/Coordinator';
 import { localisationService } from '@covid/core/localisation/LocalisationService';
+import { patientService } from '@covid/core/patient/PatientService';
 import { TProfile } from '@covid/core/profile/ProfileService';
+import { TRootState } from '@covid/core/state/root';
+import { selectStartupInfo } from '@covid/core/state/selectors';
+import { TStartupInfo } from '@covid/core/user/dto/UserAPIContracts';
 import { ScreenParamList } from '@covid/features';
 import { appCoordinator } from '@covid/features/AppCoordinator';
 import i18n from '@covid/locale/i18n';
-import { offlineService } from '@covid/services';
+import { assessmentService, offlineService } from '@covid/services';
+import { sizes } from '@covid/themes';
 import { DEFAULT_PROFILE } from '@covid/utils/avatar';
 import { NavigationProp, RouteProp } from '@react-navigation/native';
 import { colors } from '@theme';
 import * as React from 'react';
+import { useSelector } from 'react-redux';
 
 import { ProfileCard } from './components/ProfileCard';
 import { ProfileList } from './components/ProfileList';
@@ -30,6 +38,8 @@ export default function SelectProfileScreen({ navigation, route }: TProps) {
   const assessmentFlow = route.params?.assessmentFlow;
   const coordinator: TSelectProfileCoordinator = appCoordinator;
   const config = localisationService.getConfig();
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const startupInfo = useSelector<TRootState, TStartupInfo | undefined>(selectStartupInfo);
 
   React.useEffect(() => {
     return navigation.addListener('focus', listProfiles);
@@ -71,6 +81,27 @@ export default function SelectProfileScreen({ navigation, route }: TProps) {
     });
   }
 
+  async function updateAssessment(profile: TProfile) {
+    try {
+      const assessment = { health_status: 'healthy' };
+      const patientData = await patientService.getPatientDataByProfile(profile);
+      await assessmentService.completeAssessment(assessment, patientData.patientInfo!);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Something went wrong: ', error);
+      setIsSubmitting(false);
+    }
+  }
+
+  async function reportAllProfilesAreFine() {
+    setIsSubmitting(true);
+    if (profiles) {
+      await Promise.all(profiles.map((profile) => updateAssessment(profile)));
+      setIsSubmitting(false);
+      assessmentCoordinator.goToThankYouScreen();
+    }
+  }
+
   return (
     <Screen backgroundColor={colors.backgroundSecondary} testID="select-profile-screen">
       <HeaderText>
@@ -110,6 +141,17 @@ export default function SelectProfileScreen({ navigation, route }: TProps) {
         )}
         status={status}
       />
+
+      {startupInfo?.is_tester ? (
+        <BrandedButton
+          enabled={!isSubmitting}
+          loading={isSubmitting}
+          onPress={reportAllProfilesAreFine}
+          style={{ marginTop: sizes.xl }}
+        >
+          Report all profiles as "I am fine"
+        </BrandedButton>
+      ) : null}
     </Screen>
   );
 }
