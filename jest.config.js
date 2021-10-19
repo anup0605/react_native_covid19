b4190b93efd1c59a9a02ff54a912057d7ca1236d
Julia Tan <julia@joinzoe.com>
2021-10-19 15:31:32 +0100

    Working Jest tests apart from ScalableImage component. Also error with VirtualizedLists

--- jest.config.js
+++ jest.config.js
@@ -13,10 +13,7 @@ module.exports = {
   },
   transformIgnorePatterns: [
     './node_modules/reflect-metadata/Reflect.js',
-    // 'node_modules/(react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@codler/react-native-keyboard-aware-scroll-view|victory-.*)',
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-
-    // '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|victory-*)',
     '@sentry/.*',
     'sentry-expo',
   ],
