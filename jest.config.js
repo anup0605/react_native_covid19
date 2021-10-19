1b98b6e56a1f931a45433a5ef5f49dda375132c8
Julia Tan <julia@joinzoe.com>
2021-10-19 11:27:40 +0100

    WIP Jest test fix

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,10 @@ module.exports = {
   },
   transformIgnorePatterns: [
     './node_modules/reflect-metadata/Reflect.js',
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@codler/react-native-keyboard-aware-scroll-view|victory-.*)',
+    // 'node_modules/(react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*|@codler/react-native-keyboard-aware-scroll-view|victory-.*)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+
+    // '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
     '@sentry/.*',
     'sentry-expo',
   ],
